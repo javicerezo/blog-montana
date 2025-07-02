f123fbc51bc654f73f78c86c3faebcf05f5d302e
Javi Cerezo <jc.webmob@gmail.com>
2025-07-02 18:39:01 +0200

    cambio en variables de entorno de BD

--- config/db.js
+++ config/db.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: '3306',
     dialect: 'mysql',
     define: {
-        tiemstamps: false
+        timestamps: false
     },
     pool: {
         max: 5,
