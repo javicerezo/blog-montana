1f8eb2be8974b76c68f6df22fb2008e405a9147a
Javi Cerezo <jc.webmob@gmail.com>
2025-07-03 12:47:34 +0200

    cambios en variables de entorno

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 3000,
     dialect: 'mysql',
     define: {
         timestamps: false
