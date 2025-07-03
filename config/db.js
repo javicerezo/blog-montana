5f99c0b270b3da091470fcd5fb4d28a4eb4485b4
Javi Cerezo <jc.webmob@gmail.com>
2025-07-03 14:21:42 +0200

    cambio MySQL a PostgreSQL (para hacer deploy en Render.com)

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3000,
-    dialect: 'mysql',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
     define: {
         timestamps: false
     },
@@ -16,7 +16,13 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
         acquire: 30000,
         idle: 10000
     },
-    operatorAliases: false
+    operatorAliases: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 export default db;
