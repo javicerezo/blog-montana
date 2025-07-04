2447f4382f937a94fcf5c48b96afab8197b25eef
Javi Cerezo <jc.webmob@gmail.com>
2025-07-04 08:53:28 +0200

    retoque PostgreSQL

--- config/db.js
+++ config/db.js
@@ -3,10 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    protocol: 'postgres',
     define: {
         timestamps: false
     },
